c78f60f08f2cb265f8c4b8b800f25fe7af435cc0
ElzaEldarh <bbheldarhanova2201@mail.ru>
2025-07-10 18:00:45 +0300

    feat: Начала элемент фильтрации

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 import "./globals.css";
+
 
 export default function Home() {
   return (
@@ -8,7 +9,20 @@ export default function Home() {
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
-      <div style={{height:'3000px'}}></div>
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/*Фильтрация*/}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/*Список товаров*/}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">spisok</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
